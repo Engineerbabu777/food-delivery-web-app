de720c90d303ea1404ec788e38f85bfdfc98a4a3
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-09-09 12:59:56 +0500

    Add: service funcs added in user.service

--- server/apps/users/src/users.service.ts
+++ server/apps/users/src/users.service.ts
@@ -1,8 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService, JwtVerifyOptions } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+ 
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService
+  ){}
+
+
+  // register user service!
+  async register(registerDto:RegisterDto, ){
+     
+  }
+
+
+  // login user service!
+  async login(loginDto:LoginDto){
+
   }
 }
