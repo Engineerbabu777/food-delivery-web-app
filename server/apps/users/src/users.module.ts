b5d33b383865fd078cd1995157a1b52898140744
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-09-09 12:51:13 +0500

    add: setup, a bit of project reshape, graphql added, user dto-types-entities just a bit

---
+++ server/apps/users/src/users.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+import { JwtService } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService,    JwtService,],
+})
+export class UsersModule {}
